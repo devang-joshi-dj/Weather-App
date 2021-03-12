ba1260d3344b4d0c1b77b4d3675e086e23905be7
Devang Joshi <devangjoshi11@gmail.com>
2021-03-12 21:36:16 +0530

    Done with Basic Working App

--- script.ts
+++ script.ts
@@ -1,6 +1,26 @@
 window.onload = () => {
-  let long:number;
-  let lat:number;
+  const button: any = (<HTMLInputElement>document.querySelector('.button'));
+  const input: any = (<HTMLInputElement>document.querySelector('.inputValue'));
+  const name: any = (<HTMLInputElement>document.querySelector('.name'));
+  const temperature: any = (<HTMLInputElement>document.querySelector('.temp'));
+  const scale: any = (<HTMLInputElement>document.querySelector('.scale'));
+  const desc: any = (<HTMLInputElement>document.querySelector('.desc'));
+
+  let long: number;
+  let lat: number;
+  let api: string;
+  
+  input.focus();
+
+  const getSetValues = (data) => {
+    let tempValue = data['main']['temp'];
+    let nameValue = data['name'];
+    let descValue = data['weather'][0]['description'];
+    name.innerHTML = nameValue;
+    temperature.innerHTML = tempValue;
+    desc.innerHTML = descValue;
+    scale.innerHTML = 'C';
+  }
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -9,20 +29,39 @@ window.onload = () => {
       lat = position.coords.latitude;
 
       // fetching api
-      let api = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=defa6d4900ca4eeb2dfc9dfc8eac780e&units=metric';
-      fetch(api)  
+      api = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=defa6d4900ca4eeb2dfc9dfc8eac780e&units=metric';
+      fetch(api)
         .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          // var tempValue = data['main']['temp'];
-          // var nameValue = data['name'];
-          // var descValue = data['weather'][0]['description'];
-
-          // main.innerHTML = nameValue;
-          // desc.innerHTML = "Desc - " + descValue;
-          // temp.innerHTML = "Temp - " + tempValue;
-          // input.value = "";
-        })
+        .then(data => { getSetValues(data); })
+        .catch(err => alert(err));
     });
   }
+
+  temperature.addEventListener('click', () => {
+    if (scale.innerHTML == 'C') {
+      temperature.innerHTML = (temperature.innerHTML * 1.8) + 32;
+      scale.innerHTML = 'F';
+    }else
+    if (scale.innerHTML == 'F') {
+      temperature.innerHTML = (temperature.innerHTML - 32) / 1.8;
+      scale.innerHTML = 'C';
+    }
+    })
+
+  button.addEventListener('click', () => {
+    api = 'https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=defa6d4900ca4eeb2dfc9dfc8eac780e&units=metric';
+    fetch(api)
+      .then(response => response.json())
+      .then(data => { getSetValues(data) })
+      .catch(err => alert('Not Valid Input. Enter A City.'));
+  });
+
+  document.addEventListener('keyup', (e) => {
+      var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+      if (document.activeElement === input) {
+        button.click();
+      }
+    }
+  })
 }
