c2fda3002d6b376932caf701fbd3efa043a9cb47
Devang Joshi <devangjoshi11@gmail.com>
2021-03-12 23:42:29 +0530

    Added Comments

--- script.ts
+++ script.ts
@@ -10,12 +10,14 @@ window.onload = () => {
   let lat: number;
   let api: string;
   
+  // to focus input element on page load
   input.focus();
 
   const getSetValues = (data) => {
+    // function to get Values from Weather API and set them in HTML Elements
     let tempValue = data['main']['temp'];
     let nameValue = data['name'];
-    let descValue = data['weather'][0]['description'];
+    let descValue = data['weather'][0]['description'].toUpperCase();
     name.innerHTML = nameValue;
     temperature.innerHTML = tempValue;
     desc.innerHTML = descValue;
@@ -38,6 +40,7 @@ window.onload = () => {
   }
 
   temperature.addEventListener('click', () => {
+    // event listener to change temperature scale from degrees to fahrenheit and vice versa
     if (scale.innerHTML == 'C') {
       temperature.innerHTML = (temperature.innerHTML * 1.8) + 32;
       scale.innerHTML = 'F';
@@ -49,6 +52,7 @@ window.onload = () => {
     })
 
   button.addEventListener('click', () => {
+    // event listener to fetch api when button is clicked
     api = 'https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=defa6d4900ca4eeb2dfc9dfc8eac780e&units=metric';
     fetch(api)
       .then(response => response.json())
@@ -57,6 +61,7 @@ window.onload = () => {
   });
 
   document.addEventListener('keyup', (e) => {
+    // button to check whether enter key is pressed while user is focused in  
       var code = (e.keyCode ? e.keyCode : e.which);
     if (code == 13) {
       if (document.activeElement === input) {
